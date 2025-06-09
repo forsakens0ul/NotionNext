3f711bd616676b4cdf043ee3aab63545a9f37408
forsakens0ul <52342354+forsakens0ul@users.noreply.github.com>
2025-06-09 10:52:23 +0800

    Create players.ts

---
+++ data/players.ts
@@ -0,0 +1,17 @@
+import { Player } from '@/types'
+
+const players: Player[] = [
+  {
+    name: "s1mple",
+    country: "Ukraine",
+    team: "NAVI",
+    avatar: "/avatars/s1mple.jpg",
+    mouse: "Logitech G Pro X Superlight",
+    keyboard: "Wooting 60HE",
+    monitor: "Zowie XL2546K",
+    headset: "HyperX Cloud II"
+  },
+  ...
+]
+
+export default players

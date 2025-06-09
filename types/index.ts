9fd1a4c8de12d5ef436722223b8ed792595a5d8c
forsakens0ul <52342354+forsakens0ul@users.noreply.github.com>
2025-06-09 10:51:41 +0800

    Create index.ts

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+// types/index.ts
+export interface Player {
+  name: string
+  country: string
+  team: string
+  avatar: string
+  mouse: string
+  keyboard: string
+  monitor: string
+  headset: string
+}

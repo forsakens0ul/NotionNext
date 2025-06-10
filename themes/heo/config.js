8373db921f8b1c54c2d7699f05afba9f5a626506
forsakens0ul <52342354+forsakens0ul@users.noreply.github.com>
2025-06-10 11:18:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -53,7 +53,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://www.chalice.lol',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -117,11 +117,11 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-   HEO_SOCIAL_CARD_TITLE_4: '点击加入社群',
-   HEO_SOCIAL_CARD_TITLE_5: '点击加入社群',
+  HEO_SOCIAL_CARD_TITLE_1: '公众号',
+  HEO_SOCIAL_CARD_TITLE_2: '获取最新文章▶',
+  HEO_SOCIAL_CARD_TITLE_3: '不错过精彩文章',
+   HEO_SOCIAL_CARD_TITLE_4: '微信',
+   HEO_SOCIAL_CARD_TITLE_5: '扫一扫',
 
   HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
